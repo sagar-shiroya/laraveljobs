2258ec05fd1054929d48622fff460674e62034f5
Sagar Shiroya <sagar.shiroya8@gmail.com>
2022-05-28 12:32:50 +0530

    Adding empty listing conditions with if directive

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,9 @@
 <h1>{{$title}}</h1>
 
+@if(count($listings) === 0)
+    <p>No listings found!</p>
+@endif
+
 @foreach($listings as $listing)
     <h2>{{$listing['title']}}</h2>
     <p>{{$listing['description']}}</p>
